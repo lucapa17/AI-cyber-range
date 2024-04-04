4f6466ebbfb019a5a7b74e75c65d3cdfbbce3c75
Luca Pastore <lucapastore2000@gmail.com>
2024-04-04 13:12:20 +0200

    Implemented file hash check in the server to prevent duplicate analysis of the same file by the malware detector model and to avoid duplicate files during the fine-tuning process.

--- DockerServer/main.py
+++ DockerServer/main.py
@@ -10,10 +10,12 @@ from secml_malware.utils.is_valid_url import isValidUrl
 import ember
 import asyncio
 import time
+import hashlib
 
 app = FastAPI()
 
 files = []
+hash_list = []
 X_train = None
 y_train = None
 next_fine_tuning = 0
@@ -51,23 +53,49 @@ elif classifier == "emberGBDT":
         print("Loading Ember dataset..")
         if int(training_samples) > 200000:
             subset = "train"
+            file_paths = [
+                "ember2018/train_features0.jsonl",
+                "ember2018/train_features1.jsonl",
+                "ember2018/train_features2.jsonl",
+                "ember2018/train_features3.jsonl",
+                "ember2018/train_features4.jsonl",
+                "ember2018/train_features5.jsonl"
+            ]
         else:
             subset = "test"
+            file_paths = ["ember2018/test_features.jsonl"]
+
         X_train, y_train = ember.read_vectorized_features(
             "ember2018/",
             subset=subset,
             feature_version=2
         )
+        for file_path in file_paths:
+            with open(file_path, 'r') as file:
+                for line in file:
+                    sample = json.loads(line.strip())
+                    hash_value = sample.get("sha256", None)
+                    if hash_value:
+                        hash_entry = {
+                            "sha256": hash_value
+                        }
+                        hash_list.append(hash_entry)
+                        
+        hash_list = np.array(hash_list)
+        hash_list = hash_list[y_train != -1]
         X_train = X_train[y_train != -1].astype(dtype='float64')
         y_train = y_train[y_train != -1]
         
-        random_indices = np.random.choice(len(X_train), int(training_samples), replace=False)        
+        random_indices = np.random.choice(len(X_train), int(training_samples), replace=False)
+        hash_list = hash_list[random_indices]        
         X_train = X_train[random_indices]
         y_train = y_train[random_indices]
 
         print("Ember dataset loading complete.")
         print("shape X_train: ", X_train.shape)
-        print("shape y_train: ", y_train.shape) 
+        print("shape y_train: ", y_train.shape)
+        
+        hash_list = list(hash_list)  
         
         print("Loading Ember GBDT model...")
         net = CClassifierEmber(X=X_train, y=y_train)
@@ -83,32 +111,58 @@ async def analyze(request: Request):
     
     data = await request.body()
     fp = request.headers.get("Filename")
-    # from bytes to CArray
-    bytes2CArr = np.frombuffer(data, dtype=np.uint8)
-    convertedData = CArray(bytes2CArr).atleast_2d()
-    # model initializtion
-    _, confidence = net.predict(convertedData, True)
+    
+    sha256_hash = hashlib.sha256()
+    sha256_hash.update(data)
+    hash_value = sha256_hash.hexdigest()
+
+    if hash_value not in [entry['sha256'] for entry in hash_list]:
+        bytes2CArr = np.frombuffer(data, dtype=np.uint8)
+        convertedData = CArray(bytes2CArr).atleast_2d()
+        prediction, confidence = net.predict(convertedData, True)
+        confidence = confidence[0, 1].item()
+        prediction= prediction.item()
+        hash_entry = {
+            "sha256": hash_value,
+            "prediction": prediction,
+            "confidence": confidence,    
+        }
+        hash_list.append(hash_entry)
+        if fine_tuning :
+            files.append(convertedData)
+    else:
+        existing_entry_index = next((i for i, entry in enumerate(hash_list) if entry['sha256'] == hash_value))
+        existing_entry = hash_list[existing_entry_index]
+        if existing_entry.get('prediction') is not None and existing_entry.get('confidence') is not None:
+            prediction = existing_entry.get('prediction')
+            confidence = existing_entry.get('confidence')
+        else:
+            bytes2CArr = np.frombuffer(data, dtype=np.uint8)
+            convertedData = CArray(bytes2CArr).atleast_2d()
+            prediction, confidence = net.predict(convertedData, True)
+            confidence = confidence[0, 1].item()
+            prediction= prediction.item()
+            existing_entry['prediction'] = prediction
+            existing_entry['confidence'] = confidence
+            hash_list[existing_entry_index] = existing_entry
+
     count+=1
     print(f"> Request number: {count}")
-    print(f"> The file named: {fp} is a malware with confidence {confidence[0, 1].item()}")
-    conf = confidence[1][0].item()
-       
+    print(f"> The file named: {fp} is a {'malware' if prediction == 1 else 'goodware'}. Score: {confidence}")
+        
     encoded_data = {
         "file name": fp, 
-        "score": conf,
+        "score": confidence,
         "time": str(datetime.datetime.now())
     }
     with open('log_file.txt', "a") as file:
         json.dump(encoded_data, file)
         file.write('\n')
-    
-    if fine_tuning :
-        files.append(convertedData)
-        
-        if len(files) >= int(samples_for_fine_tuning) and time.time() >= next_fine_tuning:
-            asyncio.create_task(process_fine_tuning())
+       
+    if fine_tuning and len(files) >= int(samples_for_fine_tuning) and time.time() >= next_fine_tuning:
+        asyncio.create_task(process_fine_tuning())
 
-    return str(conf)
+    return str(confidence)
 
 async def process_fine_tuning():
     print("Fine tuning in progress...")
@@ -130,6 +184,11 @@ async def process_fine_tuning():
         net.classifier._fit(X=X_train, y=y_train)
 
     files.clear()
+    for entry in hash_list:
+        if 'prediction' in entry:
+            del entry['prediction']
+        if 'confidence' in entry:
+            del entry['confidence']
     print("Fine tuning completed.")
     model_version+=1
     net.classifier.save_model(f"emberGBDT_model{model_version}.txt")
@@ -149,7 +208,7 @@ def label_data(files):
     y_new = []
     for file in files:
         pred, confidence = remote_classifier.predict(file)
-        print("pred ", pred.get_data()[0], " confidence ", confidence)
-        y_new.append(pred.get_data()[0])
+        print(f"Prediction: {'malware' if pred.item() == 1 else 'goodware'}. Score: {confidence[0, 1].item()}")
+        y_new.append(pred.item())
     print("Labeling complete.")
     return y_new
